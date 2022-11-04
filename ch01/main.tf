f6c5f5ac9673f2cbd16b36f0c9cc99ae70b19bc6
Phong Chau <chaupm@9870l-208824-l.mobility.unimelb.net.au>
2022-11-04 12:55:30 +1100

    initial tf files

---
+++ ch01/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+	region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+	ami		 				= "ami-0fb653ca2d3203ac1"
+	instance_type = "t2.micro"
+
+# Adding name below. Remember capital Name
+	tags					= {
+		Name				= "terraform-example"
+	}
+}
