dde9ed52d85e4603dc36f91bda6d00aad25020ea
Phong Chau <chaupm@9870l-208824-l.mobility.unimelb.net.au>
2022-11-04 15:21:33 +1100

    onto deploying a config webserver chapter

--- ch01/main.tf
+++ ch01/main.tf
@@ -5,9 +5,33 @@ provider "aws" {
 resource "aws_instance" "example" {
 	ami		 				= "ami-0fb653ca2d3203ac1"
 	instance_type = "t2.micro"
+	vpc_security_group_ids = [aws_security_group.instance.id]
+
+
+# adding custom bash script
+	user_data			= <<-EOF
+									#!/bin/bash
+									echo "Hello world from phoodie" > index.html
+									
+									# execute native nohub cmmand to enable web server on ubuntu
+									nohup busybox httpd -f -p 8080 &
+									EOF
+	# this will destroy the orig ec2 to create this new one
+	user_data_replace_on_change = true
 
 # Adding name below. Remember capital Name
 	tags					= {
 		Name				= "terraform-example"
+	}
+}
+
+resource "aws_security_group" "instance" {
+	name = "terraform-example-instance"
+
+	ingress {
+		from_port		= 8080
+		to_port			= 8080
+		protocol		= "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
 	}
 }
