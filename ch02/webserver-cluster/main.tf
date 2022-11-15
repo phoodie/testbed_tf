32fa2ff6c8cd9c1f406d530aa72b184510d83ced
Phong Chau <chaupm@9870l-208284-l.mobility.unimelb.net.au>
2022-11-15 22:41:54 +1100

    finished deploying webserver cluster

--- ch02/webserver-cluster/main.tf
+++ ch02/webserver-cluster/main.tf
@@ -13,21 +13,35 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-0fb653ca2d3203ac1"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0fb653ca2d3203ac1"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World from phoodie " > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
+  # Required when using a launch configuration with auto scaling group
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-  user_data_replace_on_change = true
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  # addded since data source is available that we made
+  vpc_zone_identifier  = data.aws_subnets.default.ids
 
-  tags = {
-    Name = "terraform-example"
+
+  max_size             = 10
+  min_size             = 2
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
@@ -42,3 +56,17 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+#fetching data source
+data "aws_vpc" "default" {
+  default = true
+}
+
+# search filtering output from the data source "aws_vpc"
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
--- ch02/webserver-cluster/outputs.tf
+++
@@ -1,5 +0,0 @@
-# fetching output public ip
-output "public_ip" {
-  description = "fetching public ip of aws instance"
-  value       = aws_instance.example.public_ip
-}
